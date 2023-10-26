82ecd06cc578a6990fade41cb63f1391c83251a2
sebastien etienne <sebastienetienne.pro@gmail.com>
2023-10-26 12:37:33 +0200

    mentions

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,18 @@
     });
     });
 
+
+var mentionsElement = document.getElementById("mentions");
+
+function updateMentions() {
+  var currentText = mentionsElement.textContent || mentionsElement.innerText;
+  if (currentText.includes("SARL ATELIER 2D")) {
+    mentionsElement.innerHTML =
+      "© " + new Date().getFullYear() + " mentions légales";
+  } else {
+    mentionsElement.innerHTML =
+      " SARL ATELIER 2D au capital de 5000€ - SIRET 977 891 092 00016 R.C.S. Saint Brieuc <br> Code APE 71.12B police assurance SMA BTP n°7352000 / 002 158751/0 <br> Webmaster et hébergement - Etienne Design ";
+  }
+}
+
+mentionsElement.addEventListener("click", updateMentions);
